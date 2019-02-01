3d5222a9ce4378d5703a3af246910aaed01efcc8
Duncan Thacker <thackerd@Discovery.net>
2019-02-01 16:25:34 +0000

    more useful unit test

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,10 +3,18 @@
 import React from "react";
 import { mount } from "enzyme";
 import PreviewPicker from "./index";
+import { Chip } from "material-ui";
 
 describe("Preview Picker", () => {
     it("renders", () => {
-        const wrapper = mount(<PreviewPicker value={ [] } />);
-        expect(wrapper).not.toBeEmptyRender();
+        const wrapper = mount(
+            <PreviewPicker
+                value={ ["some-item"] }
+                itemToString={ item => item }
+            />
+        );
+        const itemChip = wrapper.find(Chip);
+        expect(itemChip).toHaveLength(1);
+        expect(itemChip).toHaveText("some-item");
     });
 });
